59c4d6295c72857dc57755be97ba34adccdaa1e9
Ойбек Рустамов <oybekrustamov@icloud.com>
2021-12-05 22:59:13 +0500

    zx

--- MDEV/MDEV/App.xaml.cs
+++ MDEV/MDEV/App.xaml.cs
@@ -6,7 +6,7 @@ namespace MDEV
     {
         public App()
         {
-            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("there is your syncfusion license key!");
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NTI3MTc3QDMxMzkyZTMzMmUzMGZrSWVidDk3Q3hsV1NBRHhYZWpiRFpIR25lSTRGekhMSmdUSHpxN252OTQ9");
             InitializeComponent();
 
             MainPage = new NavigationPage(new MainPage());
